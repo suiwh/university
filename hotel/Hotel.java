2e9f3c1ed3ea22cfc4cbfaf393e6807f44d9c8aa
renpeng0001 <59439243+renpeng0001@users.noreply.github.com>
2020-05-03 20:41:43 +0800

    Update Hotel.java

--- hotel/Hotel.java
+++ hotel/Hotel.java
@@ -1,1 +1,34 @@
 
+package HotelEntity;
+
+public class Hotel {
+	private String loc;
+	private String rtype;
+	private String hname;
+	private int rcost;
+	public String getLoc() {
+		return loc;
+	}
+	public void setLoc(String loc) {
+		this.loc = loc;
+	}
+	public String getRtype() {
+		return rtype;
+	}
+	public void setRtype(String rtype) {
+		this.rtype = rtype;
+	}
+	public String getHname() {
+		return hname;
+	}
+	public void setHname(String hname) {
+		this.hname = hname;
+	}
+	public int getRcost() {
+		return rcost;
+	}
+	public void setRcost(int rcost) {
+		this.rcost = rcost;
+	}	
+
+}
